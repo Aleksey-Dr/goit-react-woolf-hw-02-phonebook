b29f74f4857c8f7f8ce59f33236771711ecad03e
Aleksey-Dr <113710928+Aleksey-Dr@users.noreply.github.com>
2024-03-09 19:31:15 +0300

    prohibits adding a contact that has already been added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import { Component } from 'react';
 import { nanoid } from 'nanoid';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 import ContactForm from 'components/ContactForm';
 import Filter from 'components/Filter';
@@ -10,22 +11,15 @@ import css from './App.module.scss';
 export class App extends Component {
     state = {
         contacts: [
-            {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-            {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-            {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-            {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-          ],
-        filter: ''
+            { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+            { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+            { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+            { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+        ],
+        filter: '',
     };
 
     // ================== LOGIC
-    handlerFilter = evt => {
-        const { name, value } = evt.currentTarget;
-        this.setState({
-            [name]: value,
-        });
-    };
-
     addContact = (name, number) => {
         const contact = {
             id: nanoid(),
@@ -33,9 +27,24 @@ export class App extends Component {
             number,
         };
 
-        this.setState(prevState => ({
-            contacts: [contact, ...prevState.contacts],
-        }));
+        let includesName = false;
+        this.state.contacts.map(contact => {
+            contact.name === name && (includesName = true);
+            return includesName;
+        });
+
+        includesName
+            ? Notify.warning(name + ' is already in contacts')
+            : this.setState(({ contacts }) => ({
+                  contacts: [contact, ...contacts],
+            }));
+    };
+
+    handlerFilter = evt => {
+        const { name, value } = evt.currentTarget;
+        this.setState({
+            [name]: value,
+        });
     };
     // ================== /LOGIC
 
@@ -45,7 +54,7 @@ export class App extends Component {
         // ==================== FILTER
         const normalizedFilter = filter.toLowerCase();
         const filterContacts = contacts.filter(contact =>
-            contact.name.toLowerCase().includes(normalizedFilter)
+            contact.name.toLowerCase().includes(normalizedFilter),
         );
         // ==================== /FILTER
 
@@ -58,5 +67,5 @@ export class App extends Component {
                 <ContactList contacts={filterContacts} />
             </div>
         );
-    };
-};
+    }
+}
